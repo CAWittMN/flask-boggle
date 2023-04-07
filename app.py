6b32816136cf75dd39e05f549efb1f1d517d89f3
Casey Wittrock <caseywittrockmn@gmail.com>
2023-04-07 15:19:01 -0500

    display board logic

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
 from boggle import Boggle
+from flask import Flask, redirect, render_template, session, request, make_response
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "boopydoop"
+
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route('/')
+def home_board():
+
+    board = boggle_game.make_board()
+    session['board'] = board
+
+    return render_template('/index.html', board=board)
+
+@app.route('/check-word')
+def check_word():
